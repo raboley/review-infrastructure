44fc6ea2e6c84f08d1d3cc188255b62d4d9b94c1
Russell Boley <raboley@gmail.com>
2020-08-11 21:04:28 -0700

    wrap it in an async function?

--- .github/actions/update-github-repo-settings.js
+++ .github/actions/update-github-repo-settings.js
@@ -2,8 +2,13 @@ const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-await octokit.request("PATCH /repos/:owner/:repo", {
-    owner,
-    repo,
-    delete_branch_on_merge: true
-})
+updateRepoSettings()
+
+async function updateRepoSettings () {
+    await octokit.request("PATCH /repos/:owner/:repo", {
+        owner,
+        repo,
+        delete_branch_on_merge: true
+    })
+}
+
