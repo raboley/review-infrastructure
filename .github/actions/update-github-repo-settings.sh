61b45b2688e1b8950596a931add01c4c6fb61ede
Russell Boley <raboley@gmail.com>
2020-08-12 06:17:33 -0700

    adding github pages setup and removing non working javascript code

--- .github/actions/update-github-repo-settings.sh
+++ .github/actions/update-github-repo-settings.sh
@@ -1,6 +1,13 @@
 curl \
   -X PATCH \
-  -u raboley \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/raboley/review-infrastructure \
+  -u raboley:$API_GITHUB_TOKEN \
+  https://api.github.com/repos/$REPOSITORY \
   -d '{"delete_branch_on_merge":"true"}'
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.switcheroo-preview+json" \
+  -u raboley:$API_GITHUB_TOKEN \
+  https://api.github.com/repos/$REPOSITORY/pages \
+  -d '{"source":{"branch":"gh-pages","path":"/docs"}}'
